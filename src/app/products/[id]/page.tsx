81232e53d58fbc1e187a357370d7193bb5cc1db4
cityfarm <cityfarm19@gmail.com>
2024-11-13 09:45:19 +0700

    deploy to vercel

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,13 @@
 
-export default function Products({params}:{params:{id:string}}){
-    const {id} = params;
-    return <h1>This is products folder. Id = {id}</h1>
-}
+
+type ProductProps = {
+    params: {
+      id: string;
+    };
+  };
+  
+  export default function Products({ params }: ProductProps) {
+    const { id } = params;
+    return <h1>This is products folder. Id = {id}</h1>;
+  }
+  
