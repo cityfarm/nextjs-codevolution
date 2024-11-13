503b82e77949805bdf08b6d26878d2b3941bc17a
cityfarm <cityfarm19@gmail.com>
2024-11-13 09:41:48 +0700

    deploy to vercel

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
-export default async function Products({params}:{params:{id:string}}){
+
+export default function Products({params}:{params:{id:string}}){
     const {id} = params;
     return <h1>This is products folder. Id = {id}</h1>
 }
