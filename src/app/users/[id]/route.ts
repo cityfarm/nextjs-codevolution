6ac876c25178d072a8fa168b4a3b4d7a66d2d691
cityfarm <cityfarm19@gmail.com>
2024-11-15 21:42:35 +0700

    'ok'

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -2,11 +2,9 @@ import { users } from "../route";
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  const { id } = params;
-  const user = users.find((user) => user.id === id);
-
+  const { id } = await params;
+  const user = users.find((user) => user.id === parseInt(id));
   return Response.json(user);
-  
 }
