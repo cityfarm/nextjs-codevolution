961c523b3bac8f3350d33f348deba4b5af2b46c2
cityfarm <cityfarm19@gmail.com>
2024-11-15 22:40:53 +0700

    'ok'

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -1,25 +1,22 @@
+import { NextResponse } from "next/server";
+
 export const users = [
   { id: 1, name: "John" },
   { id: 2, name: "James" },
 ];
 
 export async function GET() {
-  return Response.json(users);
+  return NextResponse.json(users);
 }
 
 export async function POST(request: Request) {
   const user = await request.json();
   const newUser = {
-    id:  users.length + 1,
+    id: users.length + 1,
     name: user.name,
   };
 
   users.push(newUser);
 
-  return new Response(JSON.stringify(newUser),{
-    headers: {
-        "Content-Type": "application/json",
-    },
-    status: 201,
-  });
+  return NextResponse.json(newUser, { status: 201 });
 }
