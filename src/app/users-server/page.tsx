4924b6ee4bb751be2257b9c39f09218f8d4514dd
cityfarm <cityfarm19@gmail.com>
2024-11-15 22:31:24 +0700

    'ok'

--- src/app/users-server/page.tsx
+++ src/app/users-server/page.tsx
@@ -8,7 +8,7 @@ type User = {
 };
 
 export default async function UsersServer() {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -6,10 +6,11 @@ export const users = [
 export async function GET() {
   return Response.json(users);
 }
+
 export async function POST(request: Request) {
   const user = await request.json();
   const newUser = {
-    id:  (users.length + 1),
+    id:  users.length + 1,
     name: user.name,
   };
 
