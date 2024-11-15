334e0cd3ca8a80265ae59db70104f5a86149a5b7
cityfarm <cityfarm19@gmail.com>
2024-11-15 22:18:13 +0700

    'ok'

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -2,9 +2,20 @@ import { users } from "../route";
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = await params;
+  const id  = (await params).id;
   const user = users.find((user) => user.id === parseInt(id));
   return Response.json(user);
 }
+
+/*
+export default async function Page({
+  params,
+}: {  params: Promise<{ slug: string }>})
+ {
+  const slug = (await params).slug
+  return 
+  <div>My Post: {slug}</div>
+}
+*/
